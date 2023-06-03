faab73e4dad078adb337819c30c7265f078ac2b1
Amr M. Elsayed <rixtrayker@github.com>
2023-06-03 18:47:57 +0300

    Add stgringer interface & withdraw func

--- pointers/pointers_error.go
+++ pointers/pointers_error.go
@@ -1,5 +1,7 @@
 package pointers
 
+import "fmt"
+
 type Bitcoin int
 
 type Wallet struct {
@@ -9,7 +11,18 @@ type Wallet struct {
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
 
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
