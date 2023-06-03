fa533730ca71775b88bfef70db707cb36448d2d0
Amr M. Elsayed <rixtrayker@github.com>
2023-06-03 14:07:57 +0300

    Add sum tails and refactor tests

--- arrays/sum_array.go
+++ arrays/sum_array.go
@@ -17,3 +17,18 @@ func Sum(numbers []int) (result int) {
 
 	return
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums,0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	
+	return sums
+}
--- tests/sum_array_test.go
+++ tests/sum_array_test.go
@@ -7,6 +7,19 @@ import (
 )
 
 func TestSum(t *testing.T) {
+	deepCheckSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	checkSums := func(t testing.TB, got, want int, numbers []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("want '%d' but got '%d', given %v", want, got, numbers)
+		}
+	}
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 
@@ -14,21 +27,16 @@ func TestSum(t *testing.T) {
 
 		want := 15
 
-		if got != want {
-			t.Errorf("want '%d' but got '%d', given %v", want, got, numbers)
-		}
+		checkSums(t, got, want, numbers)
 	})
 
 	t.Run("collection of any numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
 		got := arrays.Sum(numbers)
-
 		want := 6
 
-		if got != want {
-			t.Errorf("want '%d' but got '%d', given %v", want, got, numbers)
-		}
+		checkSums(t, got, want, numbers)
 	})
 
 	t.Run("deep equal", func(t *testing.T) {
@@ -36,8 +44,20 @@ func TestSum(t *testing.T) {
 		got := arrays.SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
 	
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		deepCheckSums(t, got, want)
+	})
+
+	t.Run("make the sums of some slices tails", func(t *testing.T) {
+			got := arrays.SumAllTails([]int{1, 2}, []int{0, 9})
+			want := []int{2, 9}
+	
+			deepCheckSums(t, got, want)
+		})
+
+	t.Run("safely sum empty slices tails", func(t *testing.T) {
+		got := arrays.SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		deepCheckSums(t, got, want)
 	})
 }
