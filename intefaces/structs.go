5f43520b8dbef911b26e91177961ee68b5a33087
Amr M. Elsayed <rixtrayker@github.com>
2023-06-03 18:01:30 +0300

    Add duplicate area function

--- intefaces/structs.go
+++ intefaces/structs.go
@@ -1,14 +1,21 @@
 package intefaces
 
+import "math"
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+type Circle struct {
+	Radius  float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
+func RectanleArea(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
+}
+func CircleArea(circle Circle) float64 {
+	return math.Pow(circle.Radius,2) * math.Pi 
 }
