3f90838fd7b2e559cb2b2dfa68bdd97cc9997dd0
Amr M. Elsayed <rixtrayker@github.com>
2023-06-03 18:23:31 +0300

    Refactor tests into more readable way

--- intefaces/structs.go
+++ intefaces/structs.go
@@ -9,6 +9,10 @@ type Rectangle struct {
 type Circle struct {
 	Radius  float64
 }
+type Triangle struct {
+	Base  float64
+	Height  float64
+}
 
 type Shape interface {
 	Area() float64
@@ -21,6 +25,11 @@ func (r Rectangle)Perimeter() float64 {
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
+
 func (c Circle) Area() float64 {
 	return math.Pow(c.Radius,2) * math.Pi 
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
