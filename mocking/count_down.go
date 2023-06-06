66b621a98a2aa013d2fbffa2fbc9c1d8d797a646
Amr M. Elsayed <rixtrayker@github.com>
2023-06-07 01:30:06 +0300

    inject mocking sleep dependency

--- mocking/count_down.go
+++ mocking/count_down.go
@@ -3,14 +3,35 @@ package mocking
 import (
 	"fmt"
 	"io"
+
+	// "os" // main func
+	"time"
 )
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer,s Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		s.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
+
+
+// func main(){  // main package
+// 	sleeper := &DefaultSleeper{}
+// 	Countdown(os.Stdout, sleeper)
+// }
