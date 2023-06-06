64f39f17785b913e811292e618cc256108ecd0f9
Amr M. Elsayed <rixtrayker@github.com>
2023-06-06 22:52:38 +0300

    Refactor map lookup and error

--- maps/maps.go
+++ maps/maps.go
@@ -5,11 +5,13 @@ import "errors"
 
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) (string,error) {
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
-	}
-
+		return "", ErrNotFound
+	}​
 	return definition, nil
 }
