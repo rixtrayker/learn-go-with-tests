7bb301514997cff9f55695c07553221ffb71a4db
Amr M. Elsayed <rixtrayker@github.com>
2023-06-07 01:17:41 +0300

    count downs

---
+++ mocking/count_down.go
@@ -0,0 +1,16 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
+}
